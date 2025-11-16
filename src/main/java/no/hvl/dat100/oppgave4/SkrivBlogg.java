f51f8fc149cfada837e47790eaf897e9d71da21d
Gabhor04 <gahor9583@hvl.no>
2025-11-16 13:21:20 +0100

    Final

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -9,7 +9,18 @@ import no.hvl.dat100.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        boolean skrevet;
+        PrintWriter skrivar;
 
-		throw new UnsupportedOperationException(TODO.method());
+        try {
+            skrivar = new PrintWriter(mappe + filnavn);
+            skrivar.print(samling.toString());
+            skrivar.close();
+            skrevet = true;
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Fil ikkje funnet");
+            skrevet = false;
+        } return skrevet;
 	}
 }
